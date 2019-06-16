e639e90f41a19177b195d94fccaa0dec9d390739
Cedrick Kamsi <kamsicdrick@gmail.com>
2019-06-16 15:24:16 +0200

    creatin de la couche entites et la couche dao

--- src/main/java/org/sid/CatalogueServiceApplication.java
+++ src/main/java/org/sid/CatalogueServiceApplication.java
@@ -1,13 +1,51 @@
 package org.sid;
 
+import org.sid.dao.CategoryRepository;
+import org.sid.dao.ProductRepository;
+import org.sid.entities.Category;
+import org.sid.entities.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CatalogueServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CatalogueServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository, CategoryRepository categoryRepository){
+        return args -> {
+            categoryRepository.deleteAll();
+            Stream.of("C1 Ordinateurs", "C2 Imprimantes").forEach(c->{
+                categoryRepository.save(new Category(c.split(" ")[0], c.split(" ")[1], new ArrayList<>()));
+            });
+            categoryRepository.findAll().forEach(System.out::println);
+
+
+            productRepository.deleteAll();
+            Category c1 = categoryRepository.findById("C1").get();
+            Stream.of("P1", "P2", "P3", "P4").forEach(name->{
+                productRepository.save(new Product(null, name, (1+ Math.random()*1000), c1));
+            });
+
+            Category c2 = categoryRepository.findById("C2").get();
+            Stream.of("P5", "P6", "P7", "P7").forEach(name->{
+                productRepository.save(new Product(null, name, (1+ Math.random()*1000), c2));
+            });
+
+            productRepository.findAll().forEach(p->{
+                System.out.println(p.toString());
+            });
+
+
+        };
     }
 
 }
